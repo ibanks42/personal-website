4c36c342544c6ce773776a7eb48f40d9c9f4fbe5
Isaiah Banks <isaiah.banks42@gmail.com>
2025-01-14 00:28:45 -0500

    projects page list and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })]
+	integrations: [
+		react(),
+		tailwind({ applyBaseStyles: false }),
+		mdx({
+			syntaxHighlight: false,
+			rehypePlugins: [
+				/**
+				 * Adds ids to headings
+				 */
+				rehypeSlug,
+				[
+					rehypePrettyCode,
+					{
+						keepBackground: false,
+						theme: "material-theme-palenight",
+					},
+				],
+			],
+		}),
+	],
 });
+
---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const projectsCollection = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			subtitle: z.string(),
+			date: z.coerce.date(),
+			excerpt: z.string(),
+			github: z.string().optional(),
+			image: z.string().optional(),
+			tags: z.array(z.string()),
+		}),
+});
+
+export const collections = {
+	projects: projectsCollection,
+};
